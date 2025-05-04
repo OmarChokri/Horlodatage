8de08f904b617e93cec9e89e834a0ac250cd58f3
OmarChokri <omarchokri03@gmail.com>
2025-05-04 19:12:01 +0100

    les horloges en C fix matrice

--- version en C/client.c
+++ version en C/client.c
@@ -10,49 +10,41 @@
 #define MAX_MSG 2048
 #define N_PROCESSES 4
 
-// Horloge scalaire
 typedef struct {
     int scalar;
     int pid;
 } ScalarClock;
 
-// Horloge vectorielle
 typedef struct {
     int vector[N_PROCESSES];
     int pid;
 } VectorClock;
 
-// Horloge matricielle
 typedef struct {
     int matrix[N_PROCESSES][N_PROCESSES];
     int pid;
 } MatrixClock;
 
-// Structure pour gérer toutes les horloges
 typedef struct {
     ScalarClock scalar;
     VectorClock vector;
     MatrixClock matrix;
     int pid;
-    int clock_type; // 1: scalaire, 2: vectorielle, 3: matricielle
+    int clock_type;
 } Clock;
 
-// Initialisation des horloges
 void init_clock(Clock *clock, int pid, int clock_type) {
     clock->pid = pid;
     clock->clock_type = clock_type;
 
-    // Initialisation horloge scalaire
     clock->scalar.scalar = 0;
     clock->scalar.pid = pid;
 
-    // Initialisation horloge vectorielle
     for (int i = 0; i < N_PROCESSES; i++) {
         clock->vector.vector[i] = 0;
     }
     clock->vector.pid = pid;
 
-    // Initialisation horloge matricielle
     for (int i = 0; i < N_PROCESSES; i++) {
         for (int j = 0; j < N_PROCESSES; j++) {
             clock->matrix.matrix[i][j] = 0;
@@ -61,20 +53,17 @@ void init_clock(Clock *clock, int pid, int clock_type) {
     clock->matrix.pid = pid;
 }
 
-// Mise à jour de l'horloge scalaire
 void update_scalar_clock(ScalarClock *clock) {
     clock->scalar++;
     printf("Horloge scalaire mise à jour: %d\n", clock->scalar);
 }
 
-// Fusion des horloges scalaires
 void merge_scalar_clock(ScalarClock *clock, int received_scalar) {
     printf("Fusion de l'horloge scalaire locale %d avec reçue %d\n", clock->scalar, received_scalar);
     clock->scalar = (clock->scalar > received_scalar) ? clock->scalar : received_scalar;
     update_scalar_clock(clock);
 }
 
-// Mise à jour de l'horloge vectorielle
 void update_vector_clock(VectorClock *clock) {
     clock->vector[clock->pid]++;
     printf("Horloge vectorielle mise à jour: [");
@@ -85,7 +74,6 @@ void update_vector_clock(VectorClock *clock) {
     printf("]\n");
 }
 
-// Fusion des horloges vectorielles
 void merge_vector_clock(VectorClock *clock, int *received_vector) {
     printf("Fusion de l'horloge vectorielle locale [");
     for (int i = 0; i < N_PROCESSES; i++) {
@@ -105,7 +93,6 @@ void merge_vector_clock(VectorClock *clock, int *received_vector) {
     update_vector_clock(clock);
 }
 
-// Mise à jour de l'horloge matricielle
 void update_matrix_clock(MatrixClock *clock) {
     clock->matrix[clock->pid][clock->pid]++;
     printf("Horloge matricielle mise à jour:\n");
@@ -119,7 +106,6 @@ void update_matrix_clock(MatrixClock *clock) {
     }
 }
 
-// Fusion des horloges matricielles (pour messages matriciels ou vectoriels)
 void merge_matrix_clock(MatrixClock *clock, int received_matrix[N_PROCESSES][N_PROCESSES], int sender_pid, int is_vector, int *received_vector) {
     printf("Fusion de l'horloge matricielle locale avec reçue (Processus %d):\n", sender_pid);
     printf("Locale:\n");
@@ -133,7 +119,6 @@ void merge_matrix_clock(MatrixClock *clock, int received_matrix[N_PROCESSES][N_P
     }
 
     if (is_vector) {
-        // Si le message est un vecteur (ou scalaire converti en vecteur), on met à jour la ligne correspondant au sender_pid
         printf("Reçue (vecteur converti): [");
         for (int i = 0; i < N_PROCESSES; i++) {
             printf("%d", received_vector[i]);
@@ -141,12 +126,10 @@ void merge_matrix_clock(MatrixClock *clock, int received_matrix[N_PROCESSES][N_P
         }
         printf("] de Processus %d\n", sender_pid);
 
-        // Mettre à jour la ligne sender_pid dans la matrice
         for (int j = 0; j < N_PROCESSES; j++) {
             clock->matrix[sender_pid][j] = received_vector[j];
         }
 
-        // Fusionner avec les autres lignes (prendre le max pour chaque élément)
         for (int i = 0; i < N_PROCESSES; i++) {
             if (i != sender_pid) {
                 for (int j = 0; j < N_PROCESSES; j++) {
@@ -155,7 +138,6 @@ void merge_matrix_clock(MatrixClock *clock, int received_matrix[N_PROCESSES][N_P
             }
         }
     } else {
-        // Si le message est une matrice complète
         printf("Reçue (matrice):\n");
         for (int i = 0; i < N_PROCESSES; i++) {
             printf("[");
@@ -166,19 +148,24 @@ void merge_matrix_clock(MatrixClock *clock, int received_matrix[N_PROCESSES][N_P
             printf("]\n");
         }
 
-        // Fusionner en prenant le maximum de chaque élément
         for (int i = 0; i < N_PROCESSES; i++) {
             for (int j = 0; j < N_PROCESSES; j++) {
                 clock->matrix[i][j] = (clock->matrix[i][j] > received_matrix[i][j]) ? clock->matrix[i][j] : received_matrix[i][j];
             }
         }
+
+        for (int i = 0; i < N_PROCESSES; i++) {
+            if (i != sender_pid) {
+                for (int j = 0; j < N_PROCESSES; j++) {
+                    clock->matrix[i][j] = (clock->matrix[i][j] > clock->matrix[sender_pid][j]) ? clock->matrix[i][j] : clock->matrix[sender_pid][j];
+                }
+            }
+        }
     }
 
-    // Incrémenter l'élément diagonal pour l'événement de réception
     update_matrix_clock(clock);
 }
 
-// Instructions locales (5 opérations)
 void local_operations(int pid) {
     printf("Processus %d: Exécution d'opérations locales...\n", pid);
     int a = rand() % 100;
@@ -189,7 +176,6 @@ void local_operations(int pid) {
     printf("Processus %d: Résultat des opérations locales: %d\n", pid, e);
 }
 
-// Mise à jour de l'horloge en fonction du type
 void update_clock(Clock *clock) {
     switch (clock->clock_type) {
         case 1:
@@ -206,7 +192,6 @@ void update_clock(Clock *clock) {
     }
 }
 
-// Formatage du message à envoyer
 void format_message(Clock *clock, char *msg, size_t msg_size) {
     switch (clock->clock_type) {
         case 1:
@@ -228,7 +213,7 @@ void format_message(Clock *clock, char *msg, size_t msg_size) {
                 strcat(matrix_str, row);
                 if (i < N_PROCESSES - 1) strcat(matrix_str, ",");
             }
-            snprintf(msg, msg_size, "Processus %d, Horloge matricielle: %s", 
+            snprintf(msg, msg_size, "Processus %d, Horloge matricielle: [%s]", 
                      clock->pid, matrix_str);
             break;
         }
@@ -237,7 +222,6 @@ void format_message(Clock *clock, char *msg, size_t msg_size) {
     }
 }
 
-// Parsing et fusion des messages reçus
 void process_received_message(Clock *clock, char *buffer) {
     int sender_pid;
     if (sscanf(buffer, "Processus %d", &sender_pid) != 1 || sender_pid == clock->pid) {
@@ -245,7 +229,6 @@ void process_received_message(Clock *clock, char *buffer) {
         return;
     }
 
-    // Déterminer le type d'horloge du message reçu
     if (strstr(buffer, "Horloge scalaire")) {
         int received_scalar;
         if (sscanf(buffer, "Processus %d, Horloge scalaire: %d", &sender_pid, &received_scalar) != 2) {
@@ -258,14 +241,12 @@ void process_received_message(Clock *clock, char *buffer) {
                 merge_scalar_clock(&clock->scalar, received_scalar);
                 break;
             case 2: {
-                // Convertir le scalaire en vecteur pour fusion
                 int received_vector[N_PROCESSES] = {0};
                 received_vector[sender_pid] = received_scalar;
                 merge_vector_clock(&clock->vector, received_vector);
                 break;
             }
             case 3: {
-                // Convertir le scalaire en vecteur pour fusion dans la matrice
                 int received_vector[N_PROCESSES] = {0};
                 received_vector[sender_pid] = received_scalar;
                 int dummy_matrix[N_PROCESSES][N_PROCESSES] = {0};
@@ -284,7 +265,6 @@ void process_received_message(Clock *clock, char *buffer) {
 
         switch (clock->clock_type) {
             case 1: {
-                // Utiliser la composante correspondant au sender_pid comme scalaire
                 int received_scalar = received_vector[sender_pid];
                 merge_scalar_clock(&clock->scalar, received_scalar);
                 break;
@@ -299,33 +279,61 @@ void process_received_message(Clock *clock, char *buffer) {
             }
         }
     } else if (strstr(buffer, "Horloge matricielle")) {
-        int received_matrix[N_PROCESSES][N_PROCESSES];
+        int received_matrix[N_PROCESSES][N_PROCESSES] = {0};
         char *matrix_start = strstr(buffer, "Horloge matricielle: ");
         if (!matrix_start) {
             printf("Format de message matriciel invalide\n");
             return;
         }
         matrix_start += strlen("Horloge matricielle: ");
-        int parsed = sscanf(matrix_start, 
-            "[[%d,%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d]]", 
-            &received_matrix[0][0], &received_matrix[0][1], &received_matrix[0][2], &received_matrix[0][3],
-            &received_matrix[1][0], &received_matrix[1][1], &received_matrix[1][2], &received_matrix[1][3],
-            &received_matrix[2][0], &received_matrix[2][1], &received_matrix[2][2], &received_matrix[2][3],
-            &received_matrix[3][0], &received_matrix[3][1], &received_matrix[3][2], &received_matrix[3][3]);
-        if (parsed != 16) {
+
+        // Ajuster pour gérer les crochets extérieurs
+        if (*matrix_start == '[') matrix_start++; // Sauter le premier '['
+
+        // Parser manuellement les valeurs de la matrice
+        char *ptr = matrix_start;
+        for (int i = 0; i < N_PROCESSES; i++) {
+            // Sauter les caractères inutiles (crochets, virgules)
+            while (*ptr && (*ptr == '[' || *ptr == ',' || *ptr == ' ')) ptr++;
+
+            // Parser une ligne de la matrice
+            for (int j = 0; j < N_PROCESSES; j++) {
+                if (*ptr == '\0' || *ptr == ']') break;
+                received_matrix[i][j] = atoi(ptr);
+                
+                // Avancer jusqu'au prochain nombre ou délimiteur
+                while (*ptr && *ptr != ',' && *ptr != ']') ptr++;
+                if (*ptr == ',') ptr++; // Sauter la virgule
+            }
+
+            // Sauter jusqu'à la fin de la ligne
+            while (*ptr && *ptr != '[' && *ptr != '\0') ptr++;
+        }
+
+        // Vérifier si le parsing a réussi
+        int valid = 1;
+        for (int i = 0; i < N_PROCESSES; i++) {
+            for (int j = 0; j < N_PROCESSES; j++) {
+                if (received_matrix[i][j] < 0) { // Les valeurs doivent être positives ou nulles
+                    valid = 0;
+                    break;
+                }
+            }
+            if (!valid) break;
+        }
+
+        if (!valid) {
             printf("Erreur de parsing de l'horloge matricielle\n");
             return;
         }
 
         switch (clock->clock_type) {
             case 1: {
-                // Utiliser la composante diagonale du sender_pid comme scalaire
                 int received_scalar = received_matrix[sender_pid][sender_pid];
                 merge_scalar_clock(&clock->scalar, received_scalar);
                 break;
             }
             case 2: {
-                // Utiliser la ligne sender_pid comme vecteur
                 int received_vector[N_PROCESSES];
                 for (int j = 0; j < N_PROCESSES; j++) {
                     received_vector[j] = received_matrix[sender_pid][j];
@@ -355,7 +363,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    srand(time(NULL) + pid); // Seed différent pour chaque processus
+    srand(time(NULL) + pid);
     Clock clock;
     init_clock(&clock, pid, clock_type);
 
@@ -380,15 +388,10 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Boucle principale : 4 itérations
     for (int i = 0; i < 4; i++) {
-        // Instructions locales
         local_operations(pid);
-
-        // Mise à jour de l'horloge
         update_clock(&clock);
 
-        // Envoi d'un message
         char msg[MAX_MSG];
         format_message(&clock, msg, MAX_MSG);
         if (send(sock, msg, strlen(msg), 0) < 0) {
@@ -397,13 +400,11 @@ int main(int argc, char *argv[]) {
         }
         printf("Message envoyé: %s\n", msg);
 
-        // Attendre pour permettre l'échange de messages
         usleep(500000);
 
-        // Réception d'un message
         int valread = read(sock, buffer, MAX_MSG - 1);
         if (valread > 0) {
-            buffer[valread] = '\0'; // Assurer que le buffer est terminé par un caractère nul
+            buffer[valread] = '\0';
             printf("Message reçu: %s\n", buffer);
             process_received_message(&clock, buffer);
         } else if (valread == 0) {
